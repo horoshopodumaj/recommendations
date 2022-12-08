a9130ee518296acde6b16ed74248bbaa20384514
Diana Murtazina <murka8831@yandex.ru>
2022-12-09 02:12:08 +0400

    added new components

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,7 @@
 import { Route, Routes } from "react-router-dom";
+import Books from "./components/Books";
+import Films from "./components/Films";
+import Games from "./components/Games";
 import Header from "./components/Header";
 import Login from "./components/Login";
 import MainPage from "./components/MainPage";
@@ -14,6 +17,9 @@ function App() {
                 <Route path="/profile" element={<UserPage />}>
                     <Route path=":userId" element={<UserPage />} />
                 </Route>
+                <Route path="/films" element={<Films />} />
+                <Route path="/books" element={<Books />} />
+                <Route path="/games" element={<Games />} />
             </Routes>
         </div>
     );
