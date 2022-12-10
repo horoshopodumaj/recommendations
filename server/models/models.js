c54c25d660f67d4827d9de8a675d882a29f60a50
Diana Murtazina <murka8831@yandex.ru>
2022-12-11 00:06:24 +0400

    added new data for the db model

--- server/models/models.js
+++ server/models/models.js
@@ -16,5 +16,36 @@ const Review = sequelize.define("review", {
     tags: { type: DataTypes.ARRAY(DataTypes.STRING), allowNull: false },
     description: { type: DataTypes.STRING, allowNull: false },
     image: { type: DataTypes.STRING },
-    rating: { type: DataTypes.INTEGER, defaultValue: 0 },
+    rating: { type: DataTypes.INTEGER, defaultValue: 0, allowNull: false },
+});
+
+const Group = sequelize.define("group", {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    name: { type: DataTypes.STRING, unique: true, allowNull: false },
+});
+
+const Tag = sequelize.define("tag", {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    name: { type: DataTypes.STRING, unique: true, allowNull: false },
+});
+
+const Like = sequelize.define("like", {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    value: { type: DataTypes.BOOLEAN, defaultValue: false },
+});
+
+const Star = sequelize.define("star", {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    value: { type: DataTypes.INTEGER, defaultValue: 0 },
+});
+
+const Rating = sequelize.define("rating", {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    rate: { type: DataTypes.INTEGER, allowNull: false },
+});
+
+const Comment = sequelize.define("comment", {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    description: { type: DataTypes.STRING, allowNull: false },
+    date: { type: DataTypes.DATEONLY, allowNull: false },
 });
