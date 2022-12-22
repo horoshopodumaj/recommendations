0a8907be74b16d29382c994704549c485c613d4e
Diana Murtazina <murka8831@yandex.ru>
2022-12-22 14:31:40 +0400

    layout of the main page blocks

--- client/src/components/mainPage/MainPage.js
+++ client/src/components/mainPage/MainPage.js
@@ -1,8 +1,9 @@
 import React from "react";
 import style from "./MainPage.module.scss";
-import { Container, Divider, Typography } from "@mui/material";
+import { Box, Button, Container, Typography } from "@mui/material";
 import Header from "../header/Header";
 import { FormattedMessage } from "react-intl";
+import { Link } from "react-router-dom";
 
 export default function MainPage() {
     return (
@@ -37,7 +38,70 @@ export default function MainPage() {
                     </Container>
                 </div>
             </section>
-            <section style={{ height: "1000px" }}></section>
+            <section style={{ height: "1000px" }}>
+                <Container sx={{ py: "60px" }}>
+                    <Box>
+                        <Typography
+                            component="h2"
+                            sx={{
+                                fontSize: {
+                                    xs: "1.5rem",
+                                    md: "2rem",
+                                },
+                                fontWeight: 500,
+                            }}>
+                            <FormattedMessage id="latestReviews" />
+                        </Typography>
+                        <Typography
+                            component="p"
+                            sx={{
+                                fontSize: {
+                                    xs: "1rem",
+                                    md: "1.125rem",
+                                },
+                            }}>
+                            <FormattedMessage id="latestReviewsDesc" />
+                        </Typography>
+                    </Box>
+                </Container>
+            </section>
+            <section className={style.about}>
+                <div className={style.wrapperAbout}>
+                    <Container>
+                        <Box
+                            sx={{
+                                margin: "0 0 0 auto",
+                                width: { xs: "100%", md: "40%", sm: "58%" },
+                            }}>
+                            <Typography
+                                component="h3"
+                                sx={{
+                                    fontSize: "2.25rem",
+                                    fontWeight: 300,
+                                    mb: "0.5rem",
+                                    color: "white",
+                                }}>
+                                <FormattedMessage id="LetHelpYou" />
+                            </Typography>
+                            <Typography
+                                component="p"
+                                sx={{
+                                    fontSize: "1rem",
+                                    mb: "30px",
+                                    color: "white",
+                                }}>
+                                <FormattedMessage id="LetHelpYouParagraph" />
+                            </Typography>
+                            <Link to="/login">
+                                <Button className={style.button} variant="contained">
+                                    <FormattedMessage id="joinNow" />
+                                </Button>
+                            </Link>
+                        </Box>
+                    </Container>
+                </div>
+            </section>
+            <section style={{ height: "500px" }}></section>
         </>
     );
 }
--- client/src/i18n/messages.js
+++ client/src/i18n/messages.js
@@ -16,6 +16,13 @@ export const messages = {
         register: "Sign up",
         title: "Every Review is an Experience!",
         paragraph: "Check the ratings, read reviews and buy",
+        latestReviews: "Latest Reviews",
+        latestReviewsDesc: "Check out the reviews added recently",
+        LetHelpYou: "Let's Help You",
+        LetHelpYouParagraph: `This is a review platform open to everyone. Share your experiences
+        to help others make better choices. Our mission is to bring people
+        and together to create experiences for everyone.`,
+        joinNow: "Join Now",
     },
     [LOCALES.RU]: {
         films: "фильмы",
@@ -32,5 +39,12 @@ export const messages = {
         register: "Зарегистрируйтесь",
         title: "Каждый отзыв - это опыт!",
         paragraph: "Проверяйте рейтинги, читайте отзывы и покупайте",
+        latestReviews: "Последние Обзоры",
+        latestReviewsDesc: "Посмотрите обзоры добавленные недавно",
+        LetHelpYou: "Мы поможем вам",
+        LetHelpYouParagraph: `Это обзорная платформа, открытая для всех. Поделитесь своим опытом
+        чтобы помочь другим сделать лучший выбор. Наша миссия состоит в том, чтобы привлечь людей
+        и вместе создавать впечатления для всех.`,
+        joinNow: "Присоединяйтесь",
     },
 };
