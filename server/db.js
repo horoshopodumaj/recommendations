d1b0df02004159e2726c2c52485e3a141ab004e6
Diana Murtazina <murka8831@yandex.ru>
2023-01-08 15:14:26 +0400

    attempt to back up server 2

--- server/db.js
+++ server/db.js
@@ -4,4 +4,7 @@ module.exports = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process
     dialect: "postgres",
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
+    dialectOptions: {
+        ssl: false,
+    },
 });
