b87c5ff301543cfcdb9ca00baf3cc679cb787fb0
Diana Murtazina <murka8831@yandex.ru>
2023-01-09 22:48:16 +0400

    4

--- server/index.js
+++ server/index.js
@@ -29,7 +29,7 @@ app.use(cookieParser());
 
 app.use(
     cors({
-        origin: "*",
+        origin: process.env.CLIENT_URL,
         credentials: true,
         methods: ["GET", "POST", "PUT", "DELETE"],
         //optionsSuccessStatus: 200,
