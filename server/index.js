d658452074b35bd7cc9ef951017078411e5effac
Diana Murtazina <murka8831@yandex.ru>
2023-01-09 15:02:33 +0400

    &?

--- server/index.js
+++ server/index.js
@@ -48,7 +48,8 @@ app.use(
     cors({
         origin: process.env.CLIENT_URL,
         credentials: true,
-        //methods: "GET,POST,PUT,DELETE",
+        methods: "GET,POST,PUT,DELETE",
+        allowedHeaders: ["Origin", "Content-Type", "Accept"],
     })
 );
 
