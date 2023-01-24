30139691768b5735f956b11c58cd7562a8451290
Diana Murtazina <murka8831@yandex.ru>
2023-01-25 01:08:45 +0400

    the user's avatar for the comment

--- client/src/components/comment/Comment.js
+++ client/src/components/comment/Comment.js
@@ -1,12 +1,19 @@
 import { Grid, Typography } from "@mui/material";
 import React from "react";
 import { Link } from "react-router-dom";
+import UserAvatar from "../avatar/UserAvatar";
 
 export default function Comment({ comment, name }) {
     return (
-        <Grid container sx={{ paddingTop: "20px" }}>
-            <Grid item xs={0} md={3} sx={{ display: { xs: "none", md: "block" } }}>
-                Avatar
+        <Grid container spacing={2} sx={{ paddingTop: "20px" }}>
+            <Grid
+                item
+                xs={0}
+                md={3}
+                sx={{ display: { xs: "none", md: "flex" }, justifyContent: "flex-end" }}>
+                <Link to={`/profile/${comment.userId}`}>
+                    <UserAvatar width={"40px"} height={"40px"} name={name} />
+                </Link>
             </Grid>
             <Grid item xs={12} md={9}>
                 <Link to={`/profile/${comment.userId}`} style={{ color: "black" }}>
